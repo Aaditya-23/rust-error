c23dc86861a0e03afcd1a8f681cf722ba884c181
aaditya-23 <aaditya220055@gmail.com>
2025-03-19 23:12:43 +0530

    add content

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use bumpalo::{collections::Vec, Bump};
+
+enum Command<'a> {
+    Add(Vec<'a, Command<'a>>),
+    Sub(Vec<'a, Command<'a>>),
+    Break,
+}
+
+fn main() {
+    let arena = Bump::new();
+    let mut cmds = Vec::new_in(&arena);
+    action(&arena, &mut cmds);
+}
+
+fn action<'a>(arena: &'a Bump, cmds: &mut Vec<'a, Command<'a>>) {
+    let mut cmd_stack = Vec::new_in(arena);
+    action_recursive(arena,cmds, &mut cmd_stack);
+}
+
+fn action_recursive<'a>(arena: &'a Bump, cmds: &mut Vec<'a, Command<'a>>, cmd_stack: &mut Vec<'a, Vec<'a, Command<'a>>>) {
+
+    let mut breaks = Vec::new_in(arena);
+
+    cmds.iter().for_each(|cmd| {
+        if matches!(cmd, Command::Break) {
+            breaks.push(cmd)
+        }
+    });
+
+    cmd_stack.push(breaks)
+    
+}
+
